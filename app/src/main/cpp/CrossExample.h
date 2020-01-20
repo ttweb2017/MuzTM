0569918bebe0f338d1381cd8f1acd509fa6c69dd
shgini27 <go_shuhrat@yahoo.com>
2020-01-20 23:51:46 +0500

    initial commit

---
+++ app/src/main/cpp/CrossExample.h
@@ -0,0 +1,38 @@
+#ifndef Header_CrossExample
+#define Header_CrossExample
+
+#include <math.h>
+#include <pthread.h>
+#include <SuperpoweredAdvancedAudioPlayer.h>
+#include <SuperpoweredFilter.h>
+#include <SuperpoweredRoll.h>
+#include <SuperpoweredFlanger.h>
+#include <OpenSource/SuperpoweredAndroidAudioIO.h>
+
+#define HEADROOM_DECIBEL 3.0f
+static const float headroom = powf(10.0f, -HEADROOM_DECIBEL * 0.05f);
+
+class CrossExample {
+public:
+
+	CrossExample(unsigned int samplerate, unsigned int buffersize, const char *path, int fileAoffset, int fileAlength, int fileBoffset, int fileBlength);
+	~CrossExample();
+
+	bool process(short int *output, unsigned int numberOfSamples, unsigned int samplerate);
+	void onPlayPause(bool play);
+	void onCrossfader(int value);
+	void onFxSelect(int value);
+	void onFxOff();
+	void onFxValue(int value);
+
+private:
+    SuperpoweredAndroidAudioIO *output;
+    Superpowered::AdvancedAudioPlayer *playerA, *playerB;
+    Superpowered::Roll *roll;
+    Superpowered::Filter *filter;
+    Superpowered::Flanger *flanger;
+	float crossFaderPosition, volA, volB;
+	unsigned int activeFx, numPlayersLoaded;
+};
+
+#endif
---
+++ app/src/main/cpp/RecorderExample.cpp
@@ -0,0 +1,100 @@
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+#include <OpenSource/SuperpoweredAndroidAudioIO.h>
+#include <Superpowered.h>
+#include <SuperpoweredSimple.h>
+#include <SuperpoweredRecorder.h>
+#include <unistd.h>
+
+static SuperpoweredAndroidAudioIO *audioIO;
+static Superpowered::Recorder *recorder;
+
+// This is called periodically by the audio I/O.
+static bool audioProcessing (
+        void * __unused clientdata, // custom pointer
+        short int *audio,           // buffer of interleaved samples
+        int numberOfFrames,         // number of frames to process
+        int __unused samplerate     // current sample rate in Hz
+) {
+    float floatBuffer[numberOfFrames * 2];
+    Superpowered::ShortIntToFloat(audio, floatBuffer, (unsigned int)numberOfFrames);
+    recorder->recordInterleaved(floatBuffer, (unsigned int)numberOfFrames);
+    return false;
+}
+
+// StartAudio - Start audio engine.
+extern "C" JNIEXPORT void
+Java_com_voicetm_testingrecord_MainActivity_StartAudio (
+        JNIEnv *env,
+        jobject  __unused obj,
+        jint samplerate,
+        jint buffersize,
+        jstring tempPath,       // path to a temporary file
+        jstring destPath        // path to the destination file
+) {
+    Superpowered::Initialize(
+            "ExampleLicenseKey-WillExpire-OnNextUpdate",
+            false, // enableAudioAnalysis (using SuperpoweredAnalyzer, SuperpoweredLiveAnalyzer, SuperpoweredWaveform or SuperpoweredBandpassFilterbank)
+            true, // enableFFTAndFrequencyDomain (using SuperpoweredFrequencyDomain, SuperpoweredFFTComplex, SuperpoweredFFTReal or SuperpoweredPolarFFT)
+            false, // enableAudioTimeStretching (using SuperpoweredTimeStretching)
+            true, // enableAudioEffects (using any SuperpoweredFX class)
+            true, // enableAudioPlayerAndDecoder (using SuperpoweredAdvancedAudioPlayer or SuperpoweredDecoder)
+            false, // enableCryptographics (using Superpowered::RSAPublicKey, Superpowered::RSAPrivateKey, Superpowered::hasher or Superpowered::AES)
+            false  // enableNetworking (using Superpowered::httpRequest)
+    );
+
+    // Get path strings.
+    const char *temp = env->GetStringUTFChars(tempPath, 0);
+    const char *dest = env->GetStringUTFChars(destPath, 0);
+
+    // Initialize the recorder with a temporary file path.
+    recorder = new Superpowered::Recorder(temp);
+    // Start a new recording.
+    recorder->prepare(
+            dest,                     // destination path
+            (unsigned int)samplerate, // sample rate in Hz
+            true,                     // apply fade in/fade out
+            1                         // minimum length of the recording in seconds
+            );
+
+    // Release path strings.
+    env->ReleaseStringUTFChars(tempPath, temp);
+    env->ReleaseStringUTFChars(destPath, dest);
+
+    // Initialize audio engine with audio callback function.
+    audioIO = new SuperpoweredAndroidAudioIO (
+            samplerate,      // native sampe rate
+            buffersize,      // native buffer size
+            true,            // enableInput
+            false,           // enableOutput
+            audioProcessing, // process callback function
+            NULL             // clientData
+    );
+}
+
+// StopAudio - Stop audio engine and free audio buffer.
+extern "C" JNIEXPORT void
+Java_com_voicetm_testingrecord_MainActivity_StopRecording(JNIEnv * __unused env, jobject __unused obj) {
+    recorder->stop();
+    delete audioIO;
+
+    // Wait until the recorder finished writing everything to disk.
+    // It's better to do this asynchronously, but we're just blocking (sleeping) now.
+    while (!recorder->isFinished()) usleep(100000);
+
+    __android_log_print(ANDROID_LOG_DEBUG, "Recorder", "Finished recording.");
+    delete recorder;
+}
+
+// onBackground - Put audio processing to sleep if no audio is playing.
+extern "C" JNIEXPORT void
+Java_com_voicetm_testingrecord_MainActivity_onBackground(JNIEnv * __unused env, jobject __unused obj) {
+    audioIO->onBackground();
+}
+
+// onForeground - Resume audio processing.
+extern "C" JNIEXPORT void
+Java_com_voicetm_testingrecord_MainActivity_onForeground(JNIEnv * __unused env, jobject __unused obj) {
+    audioIO->onForeground();
+}
